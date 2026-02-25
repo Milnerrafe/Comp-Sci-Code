ab4a946604a989f3a8784ca3a88d2b1ff4c2751c
Rafe Milner <milnerrafe@outlook.com>
2026-02-25 14:04:52 +0800

    Create datatypes.py

---
+++ Practice/25-Feb/datatypes.py
@@ -0,0 +1,14 @@
+studentName = "alex"  # datatype = str
+
+score1 = 85  # datatype = int
+score2 = 78.5
+score3 = 92
+
+average = round((score1 + score2 + score3) / 3, 3)  # datatype = float
+
+
+passed = average >= 50  # datatype = bool
+
+print(f"Studentn Name: {studentName}")
+print(f"Average Score: {average}")
+print(f"Passed?: {passed}")

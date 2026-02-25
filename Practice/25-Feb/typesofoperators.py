225c375dcbff5c970f54638f6230056764d337a2
Rafe Milner <milnerrafe@outlook.com>
2026-02-25 14:11:48 +0800

    Create typesofoperators.py

---
+++ Practice/25-Feb/typesofoperators.py
@@ -0,0 +1,13 @@
+a = 10
+b = 3
+
+print(f"Addition: {a + b}")
+print(f"Modulus: {a % b}")
+
+age = 14
+hasPermission = True
+
+if age >= 15 and hasPermission:
+    print("Allowed to participate")
+else:
+    print("Not Allowed")
